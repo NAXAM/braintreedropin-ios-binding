10986d2374cfa265faf7442b85e87e61f1bf06cb
Vu Duc Tuyen <tuyen@naxam.net>
2017-07-10 23:33:31 +0700

    Fix linker issue

---
+++ Naxam.BraintreeDropIn.iOS/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Foundation;
+using BraintreeUnionPay;
+
+namespace BraintreeDropIn
+{
+    [Preserve(AllMembers = true)]
+    public static class Linker
+    {
+        public static void Init() {
+			new BraintreeCard.BTCard();
+			new BTCardCapabilities();
+        }
+    }
+}
